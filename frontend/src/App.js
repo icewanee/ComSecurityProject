9f82781c1908718266422f0183ef3f7ee4e63969
icewanee <iamicewanee@gmail.com>
2020-11-02 23:35:07 +0700

    add blockpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,19 @@ import ReactDOM from "react-dom";
 import { Route, Switch, Router } from "react-router-dom";
 import history from "./history";
 import LoginPage from "./page/LoginPage";
+
 import Comment from './component/Comment';
 import CommentFrom from './component/CommentForm';
-import PageNotFound from './page/PageNotFound'
-
+import PageNotFound from './page/PageNotFound';
+import BlockPage from './page/BlockPage';
 
 function App() {
   return (
     <Router history={history}>
       <div>
         <Switch>
-          <Route exact path="/" component={CommentFrom} />
-          <Route  path="/login" component={LoginPage} />
+          <Route exact path="/" component={BlockPage} />
+          <Route  path="/home" component={CommentFrom} />
           <Route component={PageNotFound} />
         </Switch>
       </div>
---
+++ frontend/src/page/BlockPage.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+
+export default class BlockPage extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            data: [
+                {
+                  title: "www.hello.ewwr.com",
+                  detail: "4"
+                },
+                {
+                 title: "www.hello.erer.com",
+                  detail: "45"
+                },
+                {
+                  title: "www.hello.dfss.com",
+                  detail: "16"
+                },
+                {
+                 title: "www.hello.oio.com",
+                 detail: "420"
+                },
+                {
+                 title: "www.hello.dfdf.com",
+                 detail: "50"
+                },
+                {
+                 title: "www.hello.iopo.com",
+                 detail: "80"
+                }
+              ]
+        }
+    }
+    
+    render() {
+        return (
+            <div align="center" style={{ paddingTop: 10}}>
+                <div className="Card">
+                        <br />
+                        <div className="row">
+                            <div className="col-md-12 ">
+                                <h2 className="card-title" style={{ fontFamily: "Courier New" }}>Topic Here</h2>
+                            </div>
+                            <br />
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <table class="table">
+                                <thead>
+                                <tr>
+                                <th scope="col" style={{ fontFamily: "Courier New" }}>topic</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                    {this.state.data.map(item => (
+                      <tr>
+                        <td style={{ fontFamily: "Courier New" }}>
+                          <div>topic : {item.title}<br />detail : {item.detail}</div></td>
+                      </tr>
+                      ))}
+                  </tbody>
+                </table>
+                </div>    
+            </div>
+          <br />
+      </div>
+      </div>
+        )
+    }
+}
+
+
