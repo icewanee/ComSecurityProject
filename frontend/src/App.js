43b63317d1aede3953eb3c03f1a80d2f9de9464a
icewanee <iamicewanee@gmail.com>
2020-11-02 23:07:25 +0700

    add commentform

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch, Router } from "react-router-dom";
 import history from "./history";
 import LoginPage from "./page/LoginPage";
 import Comment from './component/Comment';
+import CommentFrom from './component/CommentForm';
 import PageNotFound from './page/PageNotFound'
 
 
@@ -12,7 +13,7 @@ function App() {
     <Router history={history}>
       <div>
         <Switch>
-          <Route exact path="/" component={Comment} />
+          <Route exact path="/" component={CommentFrom} />
           <Route  path="/login" component={LoginPage} />
           <Route component={PageNotFound} />
         </Switch>
