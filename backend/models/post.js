687e07368133ceca14692f371467ad445231b434
saint1998 <saint1998@yahoo.com>
2020-11-08 20:22:18 +0700

    add post CRUD

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const PostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: String,
+      required: true,
+    },
   },
-  creator: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    collection: "Post",
+  }
+);
 
 const Post = (module.exports = mongoose.model("Post", PostSchema));
--- backend/services/post/index.js
+++ backend/services/post/index.js
@@ -3,20 +3,10 @@ const { handleError, ErrorHandler, HttpStatus } = require("../../utils/error");
 
 const getAllPosts = () => async (req, res) => {
   try {
-    const page = req.query.page || 1;
-    const size = req.query.size || 25;
-
-    const [_results, _count] = await Promise.all([
-      PostModel.find().skip(calSkip(page, size)).limit(size).exec(),
-      PostModel.countDocuments().exec(),
-    ]);
+    const result = await PostModel.find();
 
     return res.json({
-      currentPage: page,
-      pages: calPage(_count, size),
-      currentCount: _results.length,
-      totalCount: _count,
-      data: _results,
+      data: result,
     });
   } catch (error) {
     console.log(error);
@@ -26,21 +16,19 @@ const getAllPosts = () => async (req, res) => {
 const validateReq = (body) => {
   if (!body.title)
     throw new ErrorHandler(HttpStatus.BAD_REQUEST, "Please filled title");
-  if (!body.description)
-    throw new ErrorHandler(HttpStatus.BAD_REQUEST, "Please filled description");
 };
 
 const createPost = () => async (req, res) => {
   try {
+    if (req.user.role != "user")
+      throw new ErrorHandler(HttpStatus.FORBIDDEN, "Not have permission");
     validateReq(req.body);
-    const { title, description } = req.body;
-    const newBook = new PostModel({
-      author: req.user._id,
+    const { title } = req.body;
+    const newPost = new PostModel({
+      creator: req.user.username,
       title,
-      description,
-      createdBy: req.user._id,
     });
-    data = await PostModel.createPost(newBook);
+    data = await newPost.save();
     return res.status(HttpStatus.CREATED).json({ success: true, data });
   } catch (err) {
     return handleError(err, res);
@@ -49,7 +37,7 @@ const createPost = () => async (req, res) => {
 
 const getPostById = () => async (req, res) => {
   try {
-    return res.json({ data: await PostModel.getById(req.params.id) });
+    return res.json({ data: await PostModel.findById(req.params.id) });
   } catch (err) {
     // return handleError(err, res);
     // console.log(err);
@@ -59,21 +47,14 @@ const getPostById = () => async (req, res) => {
 
 const updatePost = () => async (req, res) => {
   try {
-    const { title, description } = req.body;
+    const { title } = req.body;
     const id = req.params.id;
 
     if (!title) {
       throw new ErrorHandler(400, "Title is null");
     }
-    if (!description) {
-      throw new ErrorHandler(400, "Description is null");
-    }
 
-    data = await PostModel.updatePost(id, {
-      title,
-      description,
-      updatedBy: req.user._id,
-    });
+    data = await PostModel.findByIdAndUpdate(id, { title });
 
     return res.json({ success: true, data });
   } catch (error) {
@@ -84,7 +65,7 @@ const updatePost = () => async (req, res) => {
 const deletePost = () => async (req, res) => {
   try {
     const id = req.params.id;
-    await PostModel.deletePost(id);
+    await PostModel.findByIdAndRemove(id);
     return res.json({ success: true });
   } catch (error) {
     console.log(error);
@@ -98,24 +79,3 @@ module.exports = {
   updatePost,
   deletePost,
 };
-
-// books = await PostModel.getAllBook();
-// _page = parseInt(req.query.page) || 1;
-// const [_results, _count] = await Promise.all([
-//   PostModel.find()
-//     .skip(req.skip)
-//     .limit(req.query.limit)
-//     .exec(),
-//   PostModel.countDocuments().exec()
-// ]);
-
-// _pages = Math.ceil(_count / req.query.limit);
-// _hasNext = paginate.hasNextPages(req)(_page);
-
-// return res.json({
-//   currentPage: _page,
-//   pages: _pages,
-//   currentCount: _results.length,
-//   totalCount: _count,
-//   data: _results
-// });
